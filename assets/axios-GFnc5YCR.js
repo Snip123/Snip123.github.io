1824c56843f4c90ab349bfe7c7f15f75f88f7045
Snip123 <37344228+Snip123@users.noreply.github.com>
2025-04-08 13:34:59 -0700

    update advanced page

--- assets/axios-GFnc5YCR.js
+++
@@ -1,1 +0,0 @@
-import{d as s,a}from"./index-Dn7259sg.js";const t=a.create({baseURL:"http://localhost:5001"}),i=s(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=t});export{t as api,i as default};
