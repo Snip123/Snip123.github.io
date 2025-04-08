9494d3dba65684623d96110b244370bcb3e14338
Snip123 <37344228+Snip123@users.noreply.github.com>
2025-04-08 13:26:49 -0700

    one more time

---
+++ assets/axios-DSzOXGTh.js
@@ -0,0 +1,1 @@
+import{d as s,a}from"./index-DOnGrVMW.js";const t=a.create({baseURL:"http://localhost:5001"}),i=s(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=t});export{t as api,i as default};
