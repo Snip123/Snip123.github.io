d0b047fd964d99204da71ae562f3c77ff464d3ea
Snip123 <37344228+Snip123@users.noreply.github.com>
2025-04-08 13:47:01 -0700

    update to use https requests

---
+++ assets/axios-CUb741E0.js
@@ -0,0 +1,1 @@
+import{d as s,a}from"./index-DQco-9m8.js";const t=a.create({baseURL:"http://localhost:5001"}),i=s(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=t});export{t as api,i as default};
