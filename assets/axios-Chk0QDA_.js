775acd97507b38a45c08e8741239261f4bd1c7e1
Snip123 <37344228+Snip123@users.noreply.github.com>
2025-04-11 13:52:36 -0700

    fix export

---
+++ assets/axios-Chk0QDA_.js
@@ -0,0 +1,1 @@
+import{d as s,a}from"./index-D6Yw8Z7F.js";const t=a.create({baseURL:"http://localhost:5001"}),i=s(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=t});export{t as api,i as default};
